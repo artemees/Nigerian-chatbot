04e67e102a0ffc9860d16158f699b4a90def5b0f
artemees <59312765+artemees@users.noreply.github.com>
2022-11-29 14:28:03 +0100

    Add files via upload

---
+++ train.py
@@ -0,0 +1,63 @@
+import json
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+from nltk_utils import tokenize, stem, bag_of_words
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+with open('intents.json', 'r') as f:
+    intents = json.load(f)
+    
+all_words = []
+tags = []
+xy = []
+for intent in intents['intents']:
+    tag = intent['tag']
+    tags.append(tag)
+    for pattern in intent['patterns']:
+        w = tokenize(pattern)
+        all_words.extend(w)
+        xy.append((w, tag))
+ignore_words = ['?', '.', '!', ',']
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+
+#sorted(set) is used to remove duplicates. we are using it for all words and will apply it to tags as well.
+
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+print(tags)
+
+x_train = []
+y_train = []
+for(pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    x_train.append(bag)
+    
+    label = tags.index(tag)
+    y_train.append(label)
+    
+    label = tags.index(tag)
+    y_train.append(label)
+    
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(x_train)
+        self.x_data = len(x_train)
+        self.y_data = len(y_train)
+        
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+    
+    def __len__(self):
+        return self.n_samples
+batch_size = 8
+
+dataset = ChatDataset()
+train_loader = DataLoader(dataset = dataset, batch_size = batch_size, shuffle = True, num_workers = 2)
