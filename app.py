4dc7e4755e087e47cdf74fec232de0926f6d2dcb
Peace Edogun <peaceedogun@gmail.com>
2022-12-31 01:27:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def get_bot_response():
     return str(processor.chatbot_response(userText))
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="10000", debug=True)
+    app.run(host="0.0.0.0")
