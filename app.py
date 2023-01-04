8c0ec701204b9b68acd50bca9c3a999d019f85e1
Peace Edogun <peaceedogun@gmail.com>
2023-01-04 20:23:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import processor
 app = Flask(__name__, template_folder='templates', static_url_path="")
 @app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index1.html', **locals())
+    return render_template('index.html', **locals())
 
 
 @app.route("/get")
